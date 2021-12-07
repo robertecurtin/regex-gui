782495a9981d18702539bce826ef3acdd89a3fbe
Robert Curtin <robertecurtin@gmail.com>
2021-12-07 10:03:58 -0500

    Got the layout working

--- src/Links.js
+++ src/Links.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import { LinkPreview } from '@dhaiwat10/react-link-preview';
+import config from './config.json'
 
-// const fs = require('fs');
-// const config = fs.readFileSync('config.txt').toString().split("\n");
-
-const Home = () => {
-  return <LinkPreview url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' width='400px' />;
-};
-const config = [
-  {
-    url: 'https://www.youtube.com/'
-  }
-]
-
+const PadRemainingSpace = <Col fluid={true} />
 function Links() {
   return <div className='Game'>
     <Container>
-      {config.map((row) => {
-        return <Row key={`row-containing-${row.url}`}>
-          <LinkPreview url={row.url} width='400px' />
-        </Row>
-      })}
+      <Row>
+        {config.map((url) => {
+          return <Col width='300px' key={`row-containing-${url}`}>
+            <LinkPreview url={url} width='300px' />
+          </Col>
+        })}
+        {PadRemainingSpace}
+      </Row>
     </Container>
   </div>;
 }
