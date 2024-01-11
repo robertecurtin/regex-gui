13381294ad4138fc57bf3aa06ef797d8e242ee5b
Robert Curtin <robertecurtin@gmail.com>
2024-01-11 15:53:34 -0500

    added misleading instructions

--- src/Intro.js
+++ src/Intro.js
@@ -5,15 +5,23 @@ import { Container, Col, Row } from 'react-bootstrap';
 const PadRemainingSpace = <Col fluid={true} />;
 
 function Intro() {
+  const [showUsage, setShowUsage] = React.useState(false);
+
   return <Alert variant='primary'>
 
-    <Container>
+    <Container onClick={() => setShowUsage(!showUsage)}>
       <Row>
         {PadRemainingSpace}
         <Col>
-          <a href='https://www.debuggex.com/cheatsheet/regex/javascript'>Javascript regex cheat sheet</a>
+          {showUsage ? "Hide" : "Show"} usage
         </Col>
         {PadRemainingSpace}
+      </Row>
+      <Row hidden={!showUsage}>
+          <a href='https://www.debuggex.com/cheatsheet/regex/javascript'>Javascript regex cheat sheet</a>
+          <Row>Fixed length: Anchors your string with ^ and $, which means the string length will be fixed (unless you use quantifiers, such as * or +) </Row>
+          <Row>Examples:</Row>
+          <Row>..a. with fixed length set searches for four-letter words with a as the third letter</Row>
       </Row>
     </Container>
   </Alert>;
